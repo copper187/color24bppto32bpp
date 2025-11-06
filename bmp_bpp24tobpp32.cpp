b6aa88ac899c2982ca8b28464ffaea56f2602598
copper187 <70820484+copper187@users.noreply.github.com>
2025-11-06 19:09:51 +0800

    Add files via upload

---
+++ bmp_bpp24tobpp32.cpp
@@ -0,0 +1,357 @@
+﻿#include <iostream>
+#include <stdint.h>
+#include <malloc.h>
+#include <ctime>
+#include <windows.h>
+#include <filesystem>
+
+#define get_aligned_boundary(mem_size, aligned_boundary) (((mem_size) + (aligned_boundary - 1)) & ~(aligned_boundary - 1))   //取最近对齐边界，快速方法
+
+extern "C"
+{
+    void bpp24to32_avx2(uint8_t* bpp24addr, uint8_t* bpp32addr, uint64_t round_count);
+    void bpp24to32_avx512vbmi1(uint8_t* bpp24addr, uint8_t* bpp32addr, uint64_t round_count);
+}
+
+void bpp24to32_pure_c(uint8_t* bpp24addr, uint8_t* bpp32addr, uint64_t pixel_count)
+{
+    uint64_t counter = 0;
+    for (;;)
+    {
+        *bpp32addr = *bpp24addr;
+        *(bpp32addr + 1) = *(bpp24addr + 1);
+        *(bpp32addr + 2) = *(bpp24addr + 2);
+        *(bpp32addr + 3) = 0xff;
+        bpp24addr += 3;
+        bpp32addr += 4;
+        counter++;
+        if (counter == pixel_count)
+        {
+            break;
+        }
+    }
+    return;
+}
+
+#if defined(_MSC_VER) && !defined(__clang__) && !defined(__GNUC__) && !defined(__INTEL_LLVM_COMPILER)
+#include <intrin.h>
+void bpp24to32_simd_init(uint8_t* bpp24addr, uint8_t* bpp32addr, uint64_t pixel_count)
+{
+    uint64_t round_count = pixel_count / 64;
+    int32_t cpuInfo[4];
+    __cpuidex(cpuInfo, 7, 0);
+    if ((cpuInfo[2] & (1 << 1)) != 0)
+    {
+        bpp24to32_avx512vbmi1(bpp24addr, bpp32addr, round_count);
+    }
+    else
+    {
+        bpp24to32_avx2(bpp24addr, bpp32addr, round_count);
+    }
+    bpp24addr += round_count * 64 * 3;
+    bpp32addr += round_count * 64 * 4;
+    pixel_count -= round_count * 64;
+    for (uint64_t i = 0; i < pixel_count; i++)
+    {
+        *bpp32addr = *bpp24addr;
+        *(bpp32addr + 1) = *(bpp24addr + 1);
+        *(bpp32addr + 2) = *(bpp24addr + 2);
+        *(bpp32addr + 3) = 0xff;
+        bpp24addr += 3;
+        bpp32addr += 4;
+    }
+    return;
+}
+
+#endif
+
+#if defined(__clang__) || defined(__GNUC__) || defined(__INTEL_LLVM_COMPILER)
+
+__attribute__((target_clones("default", "arch=haswell", "arch=broadwell", "arch=skylake", "arch=skylake-avx512", "arch=cascadelake", "arch=cooperlake", "arch=alderlake", "arch=raptorlake", "arch=meteorlake", "arch=arrowlake", "arch=arrowlake-s", "arch=lunarlake", "arch=pantherlake", "arch=sierraforest", "arch=grandridge", "arch=clearwaterforest", "arch=bdver4", "arch=znver1", "arch=znver2", "arch=znver3")))
+void bpp24to32_simd_init_avx2(uint8_t* bpp24addr, uint8_t* bpp32addr, uint64_t pixel_count)
+{
+    uint64_t round_count = pixel_count / 64;
+    bpp24to32_avx2(bpp24addr, bpp32addr, round_count);
+    bpp24addr += round_count * 64 * 3;
+    bpp32addr += round_count * 64 * 4;
+    pixel_count -= round_count * 64;
+    for (uint64_t i = 0; i < pixel_count; i++)
+    {
+        *bpp32addr = *bpp24addr;
+        *(bpp32addr + 1) = *(bpp24addr + 1);
+        *(bpp32addr + 2) = *(bpp24addr + 2);
+        *(bpp32addr + 3) = 0xff;
+        bpp24addr += 3;
+        bpp32addr += 4;
+    }
+    return;
+}
+
+#if (defined(__clang_major__) && (__clang_major__ == 21)) || (defined(__GNUC__) && (__GNUC__ == 15))
+__attribute__((target_clones("default", "arch=cannonlake", "arch=icelake-client", "arch=rocketlake", "arch=icelake-server", "arch=tigerlake", "arch=sapphirerapids", "arch=graniterapids", "arch=graniterapids-d", "arch=emeraldrapids", "arch=diamondrapids", "arch=znver4", "arch=znver5")))
+#elif (defined(__clang_major__) && (__clang_major__ <= 20)) || (defined(__GNUC__) && (__GNUC__ <= 14))
+__attribute__((target_clones("default", "arch=cannonlake", "arch=icelake-client", "arch=rocketlake", "arch=icelake-server", "arch=tigerlake", "arch=sapphirerapids", "arch=graniterapids", "arch=graniterapids-d", "arch=emeraldrapids", "arch=znver4", "arch=znver5")))
+#endif
+void bpp24to32_simd_init_avx512vbmi1(uint8_t* bpp24addr, uint8_t* bpp32addr, uint64_t pixel_count)
+{
+    uint64_t round_count = pixel_count / 64;
+    bpp24to32_avx512vbmi1(bpp24addr, bpp32addr, round_count);
+    bpp24addr += round_count * 64 * 3;
+    bpp32addr += round_count * 64 * 4;
+    pixel_count -= round_count * 64;
+    for (uint64_t i = 0; i < pixel_count; i++)
+    {
+        *bpp32addr = *bpp24addr;
+        *(bpp32addr + 1) = *(bpp24addr + 1);
+        *(bpp32addr + 2) = *(bpp24addr + 2);
+        *(bpp32addr + 3) = 0xff;
+        bpp24addr += 3;
+        bpp32addr += 4;
+    }
+    return;
+}
+
+#if (defined(__clang_major__) && (__clang_major__ >= 22)) || (defined(__GNUC__) && (__GNUC__ >= 16))
+__attribute__((target_clones("default", "arch=haswell", "arch=broadwell", "arch=skylake", "arch=skylake-avx512", "arch=cascadelake", "arch=cooperlake", "arch=cannonlake", "arch=icelake-client", "arch=rocketlake", "arch=icelake-server", "arch=tigerlake", "arch=sapphirerapids", "arch=alderlake", "arch=raptorlake", "arch=meteorlake", "arch=arrowlake", "arch=arrowlake-s", "arch=lunarlake", "arch=pantherlake", "arch=sierraforest", "arch=grandridge", "arch=graniterapids", "arch=graniterapids-d", "arch=emeraldrapids", "arch=clearwaterforest", "arch=diamondrapids", "arch=novalake", "arch=wildcatlake", "arch=bdver1", "arch=bdver2", "arch=bdver3", "arch=bdver4", "arch=znver1", "arch=znver2", "arch=znver3", "arch=znver4", "arch=znver5")))
+#elif (defined(__clang_major__) && (__clang_major__ == 21)) || (defined(__GNUC__) && (__GNUC__ == 15))
+__attribute__((target_clones("default", "arch=haswell", "arch=broadwell", "arch=skylake", "arch=skylake-avx512", "arch=cascadelake", "arch=cooperlake", "arch=cannonlake", "arch=icelake-client", "arch=rocketlake", "arch=icelake-server", "arch=tigerlake", "arch=sapphirerapids", "arch=alderlake", "arch=raptorlake", "arch=meteorlake", "arch=arrowlake", "arch=arrowlake-s", "arch=lunarlake", "arch=pantherlake", "arch=sierraforest", "arch=grandridge", "arch=graniterapids", "arch=graniterapids-d", "arch=emeraldrapids", "arch=clearwaterforest", "arch=diamondrapids", "arch=bdver1", "arch=bdver2", "arch=bdver3", "arch=bdver4", "arch=znver1", "arch=znver2", "arch=znver3", "arch=znver4", "arch=znver5")))
+#elif (defined(__clang_major__) && (__clang_major__ <= 20)) || (defined(__GNUC__) && (__GNUC__ <= 14))
+__attribute__((target_clones("default", "arch=haswell", "arch=broadwell", "arch=skylake", "arch=skylake-avx512", "arch=cascadelake", "arch=cooperlake", "arch=cannonlake", "arch=icelake-client", "arch=rocketlake", "arch=icelake-server", "arch=tigerlake", "arch=sapphirerapids", "arch=alderlake", "arch=raptorlake", "arch=meteorlake", "arch=arrowlake", "arch=arrowlake-s", "arch=lunarlake", "arch=pantherlake", "arch=sierraforest", "arch=grandridge", "arch=graniterapids", "arch=graniterapids-d", "arch=emeraldrapids", "arch=clearwaterforest", "arch=bdver1", "arch=bdver2", "arch=bdver3", "arch=bdver4", "arch=znver1", "arch=znver2", "arch=znver3", "arch=znver4", "arch=znver5")))
+#endif
+void bpp24to32_simd_init(uint8_t* bpp24addr, uint8_t* bpp32addr, uint64_t pixel_count)
+{
+    if (__builtin_cpu_supports("avx512vbmi"))
+    {
+        bpp24to32_simd_init_avx2(bpp24addr, bpp32addr, pixel_count);
+    }
+    else
+    {
+        bpp24to32_simd_init_avx512vbmi1(bpp24addr, bpp32addr, pixel_count);
+    }
+}
+#endif
+
+#pragma pack(push, 2)
+struct bmpindex
+{
+    uint16_t bfType;
+    uint32_t bfSize;
+    uint16_t zero1 = 0;
+    uint16_t zero2 = 0;
+    uint32_t bfOffBits;
+    uint32_t bfdib;
+    uint32_t biWidth;
+    uint32_t biHeight;
+    uint16_t biplane;
+    uint16_t biBitCount;
+    uint32_t zero3 = 0;
+    uint32_t zero4 = 0;
+    uint32_t zero5 = 0;
+    uint32_t zero6 = 0;
+    uint32_t zero7 = 0;
+    uint32_t zero8 = 0;
+};
+#pragma pack(pop)
+
+int main(int argc, char* argv[])
+{
+    printf("Compiler: ");
+    uint8_t is_first = 1;
+    uint8_t compiler_flags[6] = {0};
+    uint8_t isa_flags[6] = {0};
+#if defined(__CUDACC__)
+    printf("nvcc %d.%d.%d", __CUDACC_VER_MAJOR__, __CUDACC_VER_MINOR__, __CUDACC_VER_BUILD__);
+    is_first = 0;
+#endif
+#if defined(_MSC_VER)
+    if (is_first == 0)
+    {
+        printf(" + ");
+    }
+    printf("msvc %d(full version %d)", _MSC_VER, _MSC_FULL_VER);
+    is_first = 0;
+#endif
+#if defined(__clang__)
+    if (is_first == 0)
+    {
+        printf(" + ");
+    }
+    printf("clang %d.%d.%d", __clang_major__, __clang_minor__, __clang_patchlevel__);
+    is_first = 0;
+#endif
+#if defined(__INTEL_COMPILER)
+    if (is_first == 0)
+    {
+        printf(" + ");
+    }
+    printf("icc %d", __INTEL_COMPILER);
+    is_first = 0;
+#endif
+#if defined(__INTEL_LLVM_COMPILER)
+    if (is_first == 0)
+    {
+        printf(" + ");
+    }
+    printf("icx %d", __INTEL_LLVM_COMPILER);
+    is_first = 0;
+#endif
+#if defined(__GNUC__)
+    if (is_first == 0)
+    {
+        printf(" + ");
+    }
+    printf("gcc %d.%d.%d", __GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
+    is_first = 0;
+#endif
+    printf("\n");
+#if defined(__i386__) || defined(_M_IX86)
+    printf("Target Architecture: x86 (32-bit)\n");
+#endif
+#if defined(__x86_64__) || defined(_M_X64) || defined(_M_AMD64)
+    printf("Target Architecture: x86_64 / AMD64\n");
+#endif
+#if defined(__arm__) || defined(_M_ARM)
+    printf("Target Architecture: arm32 / aarch32\n");
+#endif
+#if defined(__aarch64__) || defined(_M_ARM64)
+    printf("Target Architecture: arm64 / aarch64\n");
+#endif
+#if defined(__riscv)
+    if (__riscv_xlen == 32)
+    {
+        printf("Target Architecture: riscv32\n");
+    }
+    if (__riscv_xlen == 64)
+    {
+        printf("Target Architecture: riscv64\n");
+    }
+#endif
+
+    size_t b0, b1;
+    b0 = clock();
+    uint64_t aligned_mem_size = get_aligned_boundary(sizeof(bmpindex), 128);  //寻找对齐边界
+#ifdef _WIN32
+    bmpindex* bmp_index = (bmpindex*)_aligned_malloc(aligned_mem_size, 128);
+#else
+    bmpindex* bmp_index = (bmpindex*)aligned_alloc(128, aligned_mem_size);
+#endif
+    if (!bmp_index)
+    {
+        printf("alloc bmp_index fail! bmp_index == 0(NULL)\n");
+        return 0;
+    }
+    memset(bmp_index, 0, aligned_mem_size);
+    if (!argv[1])
+    {
+        printf("no input file!\n");
+        return 0;
+    }
+    FILE* fin;
+    fin = fopen(argv[1], "rb");
+    if (!fin)
+    {
+        printf("open input file fail!\n");
+        return 0;
+    }
+    std::filesystem::path p = argv[1];
+
+    // 文件名
+    std::cout << "Filename: " << p.filename() << "\n";
+
+    fseek(fin, 0, SEEK_END);
+    uint64_t bmpsize = ftell(fin);
+    fseek(fin, 0, SEEK_SET);
+    aligned_mem_size = get_aligned_boundary(bmpsize, 128);
+#ifdef _WIN32
+    uint8_t* bpp24_pixel_buffer = (uint8_t*)_aligned_malloc(aligned_mem_size, 128);
+#else
+    uint8_t* bpp24_pixel_buffer = (uint8_t*)aligned_alloc(128, aligned_mem_size);
+#endif
+    if (!bpp24_pixel_buffer)
+    {
+        printf("alloc bmp_filestream fail! bmp_filestream == 0(NULL)\n");
+        return 0;
+    }
+    if (bmpsize > aligned_mem_size)
+    {
+        printf("alloc bmp_filestream size smaller than bmpsize!\n");
+        return 0;
+    }
+
+    fread(&bmp_index->bfType, 1, 2, fin);
+    if (bmp_index->bfType != 0x4d42)
+    {
+        printf("Read BM Magic number Error!\n");
+        return 0;
+    }
+
+    fread(&bmp_index->bfSize, 4, 1, fin);
+    fseek(fin, 4, SEEK_CUR);
+    fread(&bmp_index->bfOffBits, 4, 1, fin);
+    fseek(fin, 4, SEEK_CUR);
+    fread(&bmp_index->biWidth, 4, 1, fin);
+    fread(&bmp_index->biHeight, 4, 1, fin);
+    fseek(fin, 2, SEEK_CUR);
+    fread(&bmp_index->biBitCount, 2, 1, fin);
+
+    bmp_index->biBitCount &= 0x0000ffff;
+
+    fseek(fin, bmp_index->bfOffBits, SEEK_SET);
+
+    fread(bpp24_pixel_buffer, 1, bmpsize - (bmp_index->bfOffBits), fin);   //<--- The main read pixel function.
+
+
+    uint64_t pixel_count = (bmpsize - bmp_index->bfOffBits) / 3;
+    if (bmp_index->biBitCount != 24)   //Check valid 24bpp?
+    {
+        printf("not valid 24bpp bmp.\n");
+        return 0;
+    }
+    aligned_mem_size = get_aligned_boundary(pixel_count << 2, 128);
+#ifdef _WIN32
+    uint8_t* bpp32_pixel_buffer = (uint8_t*)_aligned_malloc(aligned_mem_size, 128);
+#else
+    uint8_t* bpp32_pixel_buffer = (uint8_t*)aligned_alloc(128, aligned_mem_size);
+#endif
+    if (!bpp32_pixel_buffer)
+    {
+        printf("alloc bpp32_pixel_buffer fail! bpp32_pixel_buffer == 0(NULL)\n");
+        return 0;
+    }
+    b1 = clock();
+
+    printf("init OK...%lldms\n", b1 - b0);
+    b0 = clock();
+    bpp24to32_simd_init(bpp24_pixel_buffer, bpp32_pixel_buffer, pixel_count);
+    b1 = clock();
+    printf("process OK...%lldms\n", b1 - b0);
+    b0 = clock();
+    uint8_t zero1 = 0;
+    uint16_t zero2 = 0;
+    uint32_t zero4 = 0;
+    uint32_t bfdib = 0x28;
+    uint16_t biplane = 1;
+    p.replace_filename(p.stem().string() + "_32bpp" + p.extension().string());
+    FILE* fout;
+    fout = fopen((char*)p.string().c_str(), "wb");
+    bmp_index->bfSize = pixel_count << 2;
+    bmp_index->bfSize += bmp_index->bfOffBits;
+    bmp_index->biBitCount = 32;
+    bmp_index->bfdib = 0x28;
+    bmp_index->biplane = 1;
+    fwrite(bmp_index, 2, 27, fout);
+    b1 = clock();
+    printf("write header time: %lld ms\n", b1 - b0);
+    fwrite(bpp32_pixel_buffer, 4, pixel_count, fout);
+    fclose(fout);
+
+#ifdef _WIN32
+    _aligned_free(bmp_index);
+    _aligned_free(bpp24_pixel_buffer);
+#else
+    free(bmp_index);
+    free(bpp24_pixel_buffer);
+#endif
+    b1 = clock();
+    printf("write file time: %lld ms\n", b1 - b0);
+}
---
+++ bpp24to32.asm
@@ -0,0 +1,173 @@
+.data 
+
+data2 SEGMENT align(1024)
+
+; 00h, 01h, 02h, 80h, 03h, 04h, 05h, 80h, 06h, 07h, 08h, 80h, 09h, 10h, 11h, ...   another style
+align 32
+shuffle_imm0 BYTE 0, 1, 2, 128, 3, 4, 5, 128, 6, 7, 8, 128, 9, 10, 11, 128, 12, 13, 14, 128, 15, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128
+shuffle_imm1 BYTE 8, 9, 10, 128, 11, 12, 13, 128, 14, 15, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 0, 1, 128, 2, 3, 4, 128, 5, 6, 7, 128
+shuffle_imm2 BYTE 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 0, 128, 1, 2, 3, 128, 4, 5, 6, 128, 7, 8, 9, 128, 10, 11, 12, 128, 13, 14, 15, 128 
+or_imm BYTE 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255
+blendvb_imm0 BYTE 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 128, 128, 0, 128, 128, 128, 0, 128, 128, 128, 0
+blendvb_imm1 BYTE 128, 128, 128, 0, 128, 128, 128, 0, 128, 128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+align 64
+vpermb_imm0 BYTE 0, 1, 2, 255, 3, 4, 5, 255, 6, 7, 8, 255, 9, 10, 11, 255, 12, 13, 14, 255, 15, 16, 17, 255, 18, 19, 20, 255, 21, 22, 23, 255
+			BYTE 24, 25, 26, 255, 27, 28, 29, 255, 30, 31, 32, 255, 33, 34, 35, 255, 36, 37, 38, 255, 39, 40, 41, 255, 42, 43, 44, 255, 45, 46, 47, 255 
+vpermb_imm1 BYTE 16, 17, 18, 255, 19, 20, 21, 255, 22, 23, 24, 255, 25, 26, 27, 255, 28, 29, 30, 255, 31, 32, 33, 255, 34, 35, 36, 255, 37, 38, 39, 255
+			BYTE 40, 41, 42, 255, 43, 44, 45, 255, 46, 47, 48, 255, 49, 50, 51, 255, 52, 53, 54, 255, 55, 56, 57, 255, 58, 59, 60, 255, 61, 62, 63, 255
+vpermt2b_imm0 BYTE 48, 49, 50, 255, 51, 52, 53, 255, 54, 55, 56, 255, 57, 58, 59, 255, 60, 61, 62, 255, 63, 64, 65, 255, 66, 67, 68, 255, 69, 70, 71, 255
+			  BYTE 72, 73, 74, 255, 75, 76, 77, 255, 78, 79, 80, 255, 81, 82, 83, 255, 84, 85, 86, 255, 87, 88, 89, 255, 90, 91, 92, 255, 93, 94, 95, 255
+vpermt2b_imm1 BYTE 32, 33, 34, 255, 35, 36, 37, 255, 38, 39, 40, 255, 41, 42, 43, 255, 44, 45, 46, 255, 47, 48, 49, 255, 50, 51, 52, 255, 53, 54, 55, 255
+			  BYTE 56, 57, 58, 255, 59, 60, 61, 255, 62, 63, 64, 255, 65, 66, 67, 255, 68, 69, 70, 255, 71, 72, 73, 255, 74, 75, 76, 255, 77, 78, 79, 255
+blendmb_imm BYTE 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255
+			BYTE 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255
+mask_imm QWORD 07777777777777777h
+data2 ends
+
+
+
+.code
+align 16
+; rcx = bpp24_addr, rdx = bpp32_addr, r8 = pixel_count
+bpp24to32_avx2 proc
+	sub rsp, 320
+	vmovdqu xmmword ptr [rsp], xmm6
+	vmovdqu xmmword ptr [rsp + 32], xmm7
+	vmovdqu xmmword ptr [rsp + 64], xmm8
+	vmovdqu xmmword ptr [rsp + 96], xmm9
+	vmovdqu xmmword ptr [rsp + 128], xmm10
+	vmovdqu xmmword ptr [rsp + 160], xmm11
+	vmovdqu xmmword ptr [rsp + 192], xmm12
+	vmovdqu xmmword ptr [rsp + 224], xmm13
+	vmovdqu xmmword ptr [rsp + 256], xmm14
+	vmovdqu xmmword ptr [rsp + 288], xmm15
+	prefetcht0 byte ptr [rcx]
+	vmovdqa ymm10, ymmword ptr [shuffle_imm0]
+	vmovdqa ymm11, ymmword ptr [shuffle_imm1]
+	vmovdqa ymm12, ymmword ptr [shuffle_imm2]
+	vmovdqa ymm13, ymmword ptr [or_imm]
+	vmovdqa ymm14, ymmword ptr [blendvb_imm0]
+	vmovdqa ymm15, ymmword ptr [blendvb_imm1]
+	align 16
+	single_round:
+		vbroadcasti128 ymm0, xmmword ptr [rcx]
+		vbroadcasti128 ymm1, xmmword ptr [rcx + 16]
+		vpshufb ymm0, ymm0, ymm10
+		vpshufb ymm1, ymm1, ymm11   ; ymm1 use again at Tag 1
+		prefetcht0 byte ptr [rcx + 64]
+		vpor ymm0, ymm0, ymm13
+		vbroadcasti128 ymm2, xmmword ptr [rcx + 32]
+		vbroadcasti128 ymm3, xmmword ptr [rcx + 48]   ; ymm3 use at Tag 2
+		vpblendvb ymm0, ymm0, ymm1, ymm14
+
+		vpshufb ymm2, ymm2, ymm12
+		vpshufb ymm3, ymm3, ymm10
+		vbroadcasti128 ymm4, xmmword ptr [rcx + 64]
+		vpor ymm2, ymm2, ymm13
+		vbroadcasti128 ymm5, xmmword ptr [rcx + 80]
+		vpblendvb ymm2, ymm2, ymm1, ymm15   ; <--- Tag 1 is here!
+
+		vmovntdq ymmword ptr [rdx], ymm0
+		vpshufb ymm4, ymm4, ymm11   ; ymm4 use again at Tag 3
+		vpshufb ymm5, ymm5, ymm12
+		prefetcht0 byte ptr [rcx + 128]
+		vpor ymm5, ymm5, ymm13
+		vbroadcasti128 ymm6, xmmword ptr [rcx + 96]
+		vbroadcasti128 ymm7, xmmword ptr [rcx + 112]   ; ymm7 use again at Tag 4
+		vpblendvb ymm3, ymm3, ymm4, ymm14   ; <--- Tag 2 is here!
+		vmovntdq ymmword ptr [rdx + 32], ymm2
+		vpshufb ymm6, ymm6, ymm10
+		vpshufb ymm7, ymm7, ymm11
+		vpblendvb ymm5, ymm5, ymm4, ymm15   ; <--- Tag 3 is here!
+
+		; 96 Bytes complete!
+
+		vpor ymm3, ymm3, ymm13
+		vpor ymm6, ymm6, ymm13
+		vmovntdq ymmword ptr [rdx + 64], ymm3
+		vbroadcasti128 ymm8, xmmword ptr [rcx + 128]
+		vbroadcasti128 ymm9, xmmword ptr [rcx + 144]   ; ymm9 use at Tag 5
+		vpblendvb ymm6, ymm6, ymm7, ymm14
+
+		vmovntdq ymmword ptr [rdx + 96], ymm5
+		vpshufb ymm8, ymm8, ymm12
+		vpshufb ymm9, ymm9, ymm10
+		prefetcht0 byte ptr [rcx + 192]
+		vpor ymm8, ymm8, ymm13
+		vmovntdq ymmword ptr [rdx + 128], ymm6
+		vpblendvb ymm8, ymm8, ymm7, ymm15   ; Tag 4 at here!
+
+		vbroadcasti128 ymm0, xmmword ptr [rcx + 160]
+		vbroadcasti128 ymm1, xmmword ptr [rcx + 176]
+		vpor ymm9, ymm9, ymm13
+		vpshufb ymm0, ymm0, ymm11
+		vpshufb ymm1, ymm1, ymm12	
+		vmovntdq ymmword ptr [rdx + 160], ymm8
+		vpor ymm1, ymm1, ymm13
+		add rcx, 192
+		vpblendvb ymm9, ymm9, ymm0, ymm14   ; <--- Tag 5 is here!
+		vpblendvb ymm1, ymm1, ymm0, ymm15
+
+		vmovntdq ymmword ptr [rdx + 192], ymm9
+		vmovntdq ymmword ptr [rdx + 224], ymm1
+		add rdx, 256
+		sub r8, 1
+		jne single_round
+	vmovdqu xmm15, xmmword ptr [rsp + 288]
+	vmovdqu xmm14, xmmword ptr [rsp + 256]
+	vmovdqu xmm13, xmmword ptr [rsp + 224]
+	vmovdqu xmm12, xmmword ptr [rsp + 192]
+	vmovdqu xmm11, xmmword ptr [rsp + 160]
+	vmovdqu xmm10, xmmword ptr [rsp + 128]
+	vmovdqu xmm9, xmmword ptr [rsp + 96]
+	vmovdqu xmm8, xmmword ptr [rsp + 64]
+	vmovdqu xmm7, xmmword ptr [rsp + 32]
+	vmovdqu xmm6, xmmword ptr [rsp]
+	add rsp, 320
+	vzeroupper
+	ret
+bpp24to32_avx2 endp
+
+align 16
+bpp24to32_avx512vbmi1 proc
+	sub rsp, 96
+	vmovdqu xmmword ptr [rsp], xmm6
+	vmovdqu xmmword ptr [rsp + 32], xmm7
+	vmovdqu xmmword ptr [rsp + 64], xmm8
+	prefetcht0 byte ptr [rcx]
+	vmovdqa64 zmm4, zmmword ptr [vpermb_imm0]
+	vmovdqa64 zmm5, zmmword ptr [vpermb_imm1]
+	vmovdqa64 zmm6, zmmword ptr [vpermt2b_imm0]
+	vmovdqa64 zmm7, zmmword ptr [vpermt2b_imm1]
+	vmovdqa64 zmm8, zmmword ptr [blendmb_imm]
+	kmovq k1, qword ptr [mask_imm]
+	align 16
+	single_round:		
+		vmovntdqa zmm0, zmmword ptr [rcx]
+		vmovntdqa zmm1, zmmword ptr [rcx + 64]
+		vmovntdqa zmm2, zmmword ptr [rcx + 128]
+		vpermb zmm3, zmm4, zmm0
+		vpermt2b zmm0{k1}{z}, zmm6, zmm1
+		vpermt2b zmm1{k1}{z}, zmm7, zmm2
+		vpermb zmm2, zmm5, zmm2
+		prefetcht0 byte ptr [rcx + 192]
+		vpblendmb zmm3{k1}, zmm8, zmm3
+		vpblendmb zmm0{k1}, zmm8, zmm0
+		vmovntdq zmmword ptr [rdx], zmm3
+		vpblendmb zmm1{k1}, zmm8, zmm1
+		vpblendmb zmm2{k1}, zmm8, zmm2
+		vmovntdq zmmword ptr [rdx + 64], zmm0
+		vmovntdq zmmword ptr [rdx + 128], zmm1
+		add rcx, 192
+		vmovntdq zmmword ptr [rdx + 192], zmm2
+		add rdx, 256
+		sub r8, 1
+		jne single_round
+	vmovdqu xmm8, xmmword ptr [rsp + 64]
+	vmovdqu xmm7, xmmword ptr [rsp + 32]
+	vmovdqu xmm6, xmmword ptr [rsp]
+	add rsp, 96
+	vzeroupper
+	ret
+bpp24to32_avx512vbmi1 endp
+end
